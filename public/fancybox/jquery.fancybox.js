2ac87bf80cd0e022f5bc986bc07267fe735f21d2
ko-saru <772726164@qq.com>
2018-01-04 11:31:12 +0800

    new

--- public/fancybox/jquery.fancybox.js
+++ public/fancybox/jquery.fancybox.js
@@ -141,9 +141,9 @@
 				image    : '<img class="fancybox-image" src="{href}" alt="">',
 				iframe   : '<iframe id="fancybox-frame{rnd}" name="fancybox-frame{rnd}" class="fancybox-iframe" frameborder="0" vspace="0" hspace="0" webkitallowfullscreen="" mozallowfullscreen="" allowfullscreen'="" +="" (ie="" ?="" '="" allowtransparency="true" :="" '')=""></iframe>',
 				error    : '<p class="fancybox-error">The requested content cannot be loaded.<br>Please try again later.</p>',
-				closeBtn : '<a title="Close" class="fancybox-item fancybox-close" href="javascript:;" target="_blank" rel="external"></a>',
-				next     : '<a title="Next" class="fancybox-nav fancybox-next" href="javascript:;" target="_blank" rel="external"><span></span></a>',
-				prev     : '<a title="Previous" class="fancybox-nav fancybox-prev" href="javascript:;" target="_blank" rel="external"><span></span></a>'
+				closeBtn : '<a title="Close" class="fancybox-item fancybox-close" href="javascript:;" target="_blank" rel="noopener"></a>',
+				next     : '<a title="Next" class="fancybox-nav fancybox-next" href="javascript:;" target="_blank" rel="noopener"><span></span></a>',
+				prev     : '<a title="Previous" class="fancybox-nav fancybox-prev" href="javascript:;" target="_blank" rel="noopener"><span></span></a>'
 			},
 
 			// Properties for each animation type
